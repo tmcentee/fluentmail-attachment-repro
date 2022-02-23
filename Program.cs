1a245f407d5a5aa205c9149303a4099a032a5ff4
Tyler McEntee <tmcentee@stackoverflow.com>
2022-02-23 08:52:40 -0600

    Small test with three distinct PDFs

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System.Net;
+using System.Net.Mail;
+
+SmtpSender sender = new SmtpSender(() => new SmtpClient(host: "smtp.gmail.com")
+{
+    EnableSsl = true,
+    UseDefaultCredentials = false,
+    DeliveryMethod = SmtpDeliveryMethod.Network,
+    Credentials = new NetworkCredential("email", "password"), //TODO: add real user/password here
+    Port = 587
+});
+
+Email.DefaultSender = sender;
+IFluentEmail newEmail = Email
+    .From("from_address") //TODO: Update From/To addresses here
+    .To("to_address")
+    .Subject("Attaching PDF repro")
+    .Body("body text");
+
+var attachments = new List<FluentEmail.Core.Models.Attachment>();
+
+var pdfList = new List<string>
+{
+    "./1.pdf",
+    "./2.pdf",
+    "./3.pdf"
+};
+
+foreach (string path in pdfList)
+{
+
+    string fileName = Path.GetFileName(path);
+
+    var attachment = new FluentEmail.Core.Models.Attachment
+    {
+
+        Data = File.OpenRead(path),
+        ContentType = "application/pdf",
+        Filename = $"{fileName}"
+    };
+
+    attachments.Add(attachment);
+    Console.WriteLine(path);
+}
+
+newEmail.Attach(attachments);
+attachments.Clear();
+FluentEmail.Core.Models.SendResponse result = await newEmail.SendAsync();
